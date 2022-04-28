68ad123441a2d26c1e0cea09fe75c3ae2ba320f0
andrea.zago <npm>
2022-04-29 01:18:19 +0200

    feat: footer icons

--- src/organisms/MobileNavigator.tsx
+++ src/organisms/MobileNavigator.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
+import Icon from "../atoms/icon";
 
 const MobileNavigator = () => {
   return (
@@ -8,7 +9,7 @@ const MobileNavigator = () => {
       {menuOptions.map((option) => (
         <Link href={option.href}>
           <MobileNavigatorContainerItem key={option.id}>
-            {option.name}
+            <Icon icon={option.icon} />
           </MobileNavigatorContainerItem>
         </Link>
       ))}
@@ -19,10 +20,10 @@ const MobileNavigator = () => {
 export default MobileNavigator;
 
 const menuOptions = [
-  { id: "1", name: "H", icon: "home", href: "/" },
-  { id: "2", name: "C", icon: "home", href: "/categories" },
-  { id: "3", name: "Y", icon: "home", href: "/" },
-  { id: "4", name: "S", icon: "Settings", href: "/settings" },
+  { id: "1", name: "Home", icon: "home", href: "/" },
+  { id: "2", name: "Categories", icon: "colours", href: "/categories" },
+  { id: "3", name: "Favourites", icon: "heart", href: "/" },
+  { id: "4", name: "Settings", icon: "cog", href: "/settings" },
 ];
 
 export const MobileNavigatorContainer = styled.div`
