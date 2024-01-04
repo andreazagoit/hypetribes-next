2d16e68d1f4fabe5f15fa87aae0a6328518784a4
Andrea Zago <a.zago@goreply.io>
2024-01-04 03:07:29 +0100

    feature: inital build added graphql playground

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -3,10 +3,13 @@ import { ApolloServer } from "@apollo/server";
 import { NextRequest } from "next/server";
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
 // Typescript: req has the type NextRequest
