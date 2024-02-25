34bcfce6ec21b8b4ae9f4c3e75e9c377decd27ba
Andrea Zago <a.zago@goreply.io>
2024-02-25 17:51:01 +0100

    bugfix: removed container old

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,14 @@
-import Container from "@/components/Container";
 import Link from "next/link";
 import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <Container>
+    <div>
       <h1>Page not found</h1>
       <Link href="/">
         <button>Back to home</button>
       </Link>
-    </Container>
+    </div>
   );
 };
 
