457843d1e13d280abc3558500e694d8b238ddff3
Andrea Zago <a.zago@goreply.io>
2024-02-25 17:57:52 +0100

    bugfix: error graphql

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { ApolloServer } from "@apollo/server";
 import { NextRequest } from "next/server";
