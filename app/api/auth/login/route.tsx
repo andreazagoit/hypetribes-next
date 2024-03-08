868e6f31c4aa1701cd784cdbd6af69427086cfff
Andrea Zago <a.zago@goreply.io>
2024-03-08 04:02:45 +0100

    test: deploy

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -45,12 +45,7 @@ export async function POST(request: NextRequest) {
     maxAge: expiresIn,
     httpOnly: true,
     secure: true,
-    path: "/",
-    domain: `${
-      process.env.NODE_ENV === "production"
-        ? `hypetribes.com`
-        : "http://localhost:3000"
-    }`,
+    sameSite: "none",
   });
 
   return NextResponse.json({
